fe00ded5b65d5088cc477b0daf49b675fb2adddc
atisshirazi <atis@eyevip.ch>
2022-12-16 10:47:53 +0100

    update java script

--- JavaScript.js
+++ JavaScript.js
@@ -19,3 +19,31 @@ async function addPicture() {
         )
 }
 
+//Checks if the email is Valid with the RegExp
+function emailChanged(){
+    var errorMessage = document.getElementById("errorMessage")
+    var email = document.getElementById("email");
+    var emailRegex = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    if (emailRegex.test(email.value)){
+        errorMessage.innerHTML= "Valid";
+        buttonEnabledEmail = true
+        checkValid();
+    }
+    else {
+        errorMessage.innerHTML= "This is Not An email";
+        buttonEnabledEmail = false
+        checkValid();
+    }
+}
+
+function passwordChanged() {
+
+    var strength = document.getElementById('strengthBorder');
+    //Looks for The Strength of a password
+    // length >10 plus Capitalletter plus number + special character
+    var strongRegex = new RegExp("^(?=.{10,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    //Length >= 8 plus Capital letter or number or special character
+    var mediumRegex = new RegExp("^(?=.{8})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+    //Length is >= 8
+    var enoughRegex = new RegExp("(?=.{8,}).*", "g");
+    var pwd = document.getElementById("password");
